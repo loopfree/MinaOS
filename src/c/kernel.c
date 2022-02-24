a8a75760db52ddb226300dfec023c392f08f6ca2
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-02-24 23:33:14 +0700

    readstring minus testing

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -33,7 +33,51 @@ void handleInterrupt21(int AX, int BX, int CX, int DX) {
 
 void printString(char *string){}
 
-void readString(char *string){}
+void readString(char *string)
+{
+	int i=0;
+	char input = 0;
+
+	while(1)
+	{
+		input = interrupt(0x16, 0, 0, 0, 0);
+
+		// input enter
+		if (input == 0xd)
+		{
+			break;
+		}
+
+		// input backspace
+		if (input == 0x8)
+		{
+			// kalau input backspace tidak di awal
+			if (i!=0)
+			{
+				interrupt (0x10, 0x0e * 256 + input, 0, 0, 0);
+				i -= 1;
+			}
+			// else
+			// Kalau input backspace diawal (i=0) abaikan saja
+		}
+
+		// input another ascii
+		else
+		{
+			string[i] = input;
+			interrupt(0x10, 0x0e * 256 + input, 0, 0, 0);
+			i += 1;
+		}
+	}
+
+	string[i] = 0x0;
+	string[i+1] = 0xa;
+
+	// enter biar rapih
+	string[i+2] = 0xd;
+
+	return;
+}
 
 void clearScreen() {
 	int i = 0;
