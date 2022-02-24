7f5e3cad00f9f427ab5372d21965489bd630eeb2
VieriMansyl <vierimansyl23@gmail.com>
2022-02-24 22:49:54 +0700

    modif kernel.c

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -7,16 +7,32 @@
 #include "header/kernel.h"
 
 int main() {
-	char *string = "Hai"; // Deklarasikan variabel pada awal scope
-	int i = 0;
-	for (i = 0; i < 3; i++) {
-		byte warna = 0xD;
-		putInMemory(0xB000, 0x8000 + 2*i, string[i]);
-		putInMemory(0xB000, 0x8001 + 2*i, warna);
-	}
-	while (true);
+
+	char buf[128];
+	clearScreen();
+	makeInterrupt21();
+	
+	readString(buf);
+	printString(buf);
+	
+	while(true);
 }
 
 void handleInterrupt21(int AX, int BX, int CX, int DX) {
-	// Definisi kosong
+	switch (AX) {
+	    case 0x0:
+	      printString(BX);
+	      break;
+	    case 0x1:
+	      readString(BX);
+	      break;
+	    default:
+	      printString("Invalid interrupt");
+	}
 }
+
+void printString(char *string){}
+
+void readString(char *string){}
+
+void clearScreen(){}
